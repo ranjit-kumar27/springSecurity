50018ad7b72c2596383a73e4dcd73069cd264128
ranjit-Kumar27 <ranjitkumarak147@gmail.com>
2026-05-27 19:19:32 +0530

    Create user entity

---
+++ src/main/java/com/example/springSecurity/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.springSecurity.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
